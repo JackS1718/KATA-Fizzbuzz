be8fb899e9d3958dbf27bab0910f648c8d1baed6
jacks1718 <javak.maravi@gmail.com>
2024-06-19 00:11:26 +0200

    update fizzbuzz

---
+++ src/main/java/com/fizzbuzz/fizzbuzz.java
@@ -0,0 +1,20 @@
+package com.fizzbuzz;
+
+public final class fizzbuzz {
+   
+    public static void main(String[] args) {
+
+for (int i=0; i< 100 ; i ++) {
+       
+         while (i <=100) 
+
+         i ++;  
+}
+
+    System.out.println(i + 1);
+
+}
+}
+
+
+       
